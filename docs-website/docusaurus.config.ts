a53367e8134302d8deec7ffa2be42f6162fe8afb
riteshshukla04 <riteshshukla2381@gmail.com>
2026-04-02 15:54:08 +0530

    chore: fix

--- docs-website/docusaurus.config.ts
+++ docs-website/docusaurus.config.ts
@@ -11,10 +11,10 @@ const config: Config = {
     v4: true,
   },
 
-  url: 'https://margelo.github.io',
-  baseUrl: '/react-native-nitro-fetch/',
+  url: 'https://fetch.riteshshukla.in',
+  baseUrl: '/',
 
-  organizationName: 'margelo',
+  organizationName: 'riteshshukla',
   projectName: 'react-native-nitro-fetch',
 
   onBrokenLinks: 'throw',
